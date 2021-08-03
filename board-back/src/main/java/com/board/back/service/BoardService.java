4f929a7676e1a3f2322941c9bb6a9a931996ca21
ljs3536 <wlstjr320@naver.com>
2021-08-03 18:38:06 +0900

    210803 board read update

---
+++ board-back/src/main/java/com/board/back/service/BoardService.java
@@ -0,0 +1,35 @@
+package com.board.back.service;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.board.back.exception.ResourceNotFoundException;
+import com.board.back.model.Board;
+import com.board.back.repository.BoardRepository;
+
+@Service
+public class BoardService {
+
+	@Autowired
+	private BoardRepository boardRepository;
+	
+	// get boards data
+	public List<Board> getAllBoard() {
+		return boardRepository.findAll();
+	}
+
+	// create board
+	public Board createBoard(Board board) {
+		return boardRepository.save(board);
+	}
+
+	// get board one by id
+	public ResponseEntity<Board> getBoard(Integer no) {
+		Board board = boardRepository.findById(no)
+				.orElseThrow(() -> new ResourceNotFoundException("Not exist Board Data by no : ["+no+"]"));
+		return ResponseEntity.ok(board);
+	}
+
+}
