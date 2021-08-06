a2e595b866bf4615afb2896470221360bbc8eed6
ljs3536 <wlstjr320@naver.com>
2021-08-06 18:02:14 +0900

    0806 repository

---
+++ board-back/src/main/java/com/board/back/repository/BoardPagingAndSortingRepository.java
@@ -0,0 +1,12 @@
+package com.board.back.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import com.board.back.model.Board;
+
+public interface BoardPagingAndSortingRepository extends PagingAndSortingRepository<Board, Long>{
+	
+	Page<Board> findAll(Pageable pageable);
+
+}
